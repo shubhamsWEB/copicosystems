ca8bac81e450f4efc660215a0c9c4c089788c292
Shubham Agrawal <shubham.agrawal.peopleinteractive.in>
2023-08-28 01:46:44 +0530

    config changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // reactStrictMode: true,
+  output: 'export',
+// assetPrefix: './',
+  trailingSlash: true,
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
